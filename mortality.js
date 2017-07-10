f8edbcba515a4dbea66004138a1d6e588b20e8ab
James <james.srn@gmail.com>
2017-07-10 14:05:57 +0100

    Create mortality.js

---
+++ mortality.js
@@ -0,0 +1,31 @@
+var people = [
+  {
+    handle: "jsrn",
+    birthday: "1990-05-22",
+    url: "http://jsrn.net"
+  }
+]
+
+$(function() {
+  for (var i = 0; i < people.length; i++) {
+    var data = people[i];
+    var $link = $("<a/>").attr("target", "_blank").attr("href", data.url);
+    var $person = $("<div/>")
+      .attr("data-birthday", data.birthday)
+      .addClass("person").appendTo($link)
+      .attr("title", data.handle);
+    $link.appendTo("body");
+  }
+  
+  $(".person").each(function() {
+    var birthday = $(this).attr("data-birthday");
+    var then = moment(birthday);
+    var now = moment();
+    var daysSoFar = now.diff(then, 'days');
+    var daysTotal = 29638;
+    var fraction = daysSoFar / daysTotal;
+    var hue = 255 - (fraction * 255);
+    hue = hue.toFixed(0);
+    $(this).css("background-color", "rgb(" + hue + "," + hue + "," + hue + ")");
+  });
+});
